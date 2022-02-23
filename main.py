45505a0f039d2d523138b99f58ba4e7f374201c7
K1ddong <bw120811@naver.com>
2022-02-24 01:19:23 +0900

    기본 시각화 및 웹앱 구축(플라스크 연결)

--- main.py
+++ main.py
@@ -30,6 +30,13 @@ rising_related_keywords = google.rising()
 top_related_keywords = google.top()
 ## 키워드 검색 추이
 google_trend = google.trends()
+### 월별로 축소
+google_trend = google_trend.reset_index().groupby(google_trend.reset_index()['date'].dt.to_period('M')).mean()
+google_trend.reset_index(inplace=True)
+
+### 수치 정규화 (최대치 기준으로)
+df = google_trend[keyword_en]
+google_trend[keyword_en] = df/df.max() * 100 
 
 #쇼피 키워드 상품 정보
 shopee_item_info = shopee_crawler.main(keyword_en)
@@ -44,19 +51,87 @@ naver_item_info = naver_shopping_crawler.main(keyword)
 naver_trend = naver_trends.main(keyword,NAVER_API_ID, NAVER_API_SECRET)
 
 
+#네이버 구글 검색 추이 통합
+naver_trend[keyword_en] = google_trend[keyword_en]
 '''
 ------------------------ 웹앱 --------------------------------
 '''
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table as dt
+# import dash_core_components as dcc
+# import dash_html_components as html
 
-# # flask server 
-# application = flask.Flask(__name__) 
+from dash import html,dcc,dash_table as dt,Dash
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+import flask
+from werkzeug.serving import run_simple
 
-# # dash app with flask server 
-# dash_app1 = Dash(__name__, server=application, url_base_pathname='/dashapp1/')
+# flask server 
+application = flask.Flask(__name__) 
+
+# dash app with flask server 
+dash_app1 = Dash(__name__, server=application, url_base_pathname='/dashapp1/')
+
+# flask app
+@application.route('/')
+def index():
+    #print('flask app index()')
+    return 'OK'
+
+dash_app1.layout = html.Div(
+    # Header Message
+    children=[
+        html.H1(children="Temp Analytics",),
+        html.P(
+            children="Temp",
+        ),
+        dt.DataTable(
+            keyword_search_volume.to_dict('records'),
+            [{"name":i, "id":i} for i in keyword_search_volume.columns]
+        ),
+        dt.DataTable(
+            top_10_related_keywords.to_dict('records'),
+            [{"name":i, "id":i} for i in top_10_related_keywords.columns]
+        ),
+        dt.DataTable(
+            naver_item_info.to_dict('records'),
+            [{"name":i, "id":i} for i in naver_item_info.columns]
+        ),
+        dt.DataTable(
+            shopee_item_info.to_dict('records'),
+            [{"name":i, "id":i} for i in shopee_item_info.columns]
+        ),
+        # 그래프		
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": naver_trend["날짜"],
+                        "y": naver_trend[keyword],
+                        "type": "lines", "name":"naver"
+                    },
+                    {    
+                        "x": naver_trend["날짜"],
+                        "y": naver_trend[keyword_en],
+                        "type": "lines", "name":"google"
+                    },
+                ],
+                "layout": {"title": "Title_1"},
+            },
+        ),
+        dt.DataTable(
+            rising_related_keywords.to_dict('records'),
+            [{"name":i, "id":i} for i in rising_related_keywords.columns]
+        ),
+        dt.DataTable(
+            top_related_keywords.to_dict('records'),
+            [{"name":i, "id":i} for i in top_related_keywords.columns]
+        ),
+        dt.DataTable(
+            naver_trend.to_dict('records'),
+            [{"name":i, "id":i} for i in naver_trend.columns]
+        ),
+    ]
+)
+
 
 
 # #dash app with pandas
@@ -67,46 +142,78 @@ import dash_table as dt
 #     html.Br(),
 #     dcc.Graph(id='time-seiries')
 # ])
-app = dash.Dash(__name__)
+# app = Dash(__name__)
 
-app.layout = html.Div(
-    # Header Message
-    children=[
-        html.H1(children="Temp Analytics",),
-        html.P(
-            children="Temp",
-        ),
-        # 그래프		
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": naver_trend["날짜"],
-                        "y": naver_trend[keyword],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Title_1"},
-            },
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": google_trend["date"],
-                        "y": google_trend[keyword_en],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Title_1"},
-            },
-        ),
-        dt.DataTable(
-            top_10_related_keywords.to_dict('records'),
-            [{"name":i, "id":i} for i in top_10_related_keywords.columns]
-        ),
-    ]
-)
-
+# app.layout = html.Div(
+#     # Header Message
+#     children=[
+#         html.H1(children="Temp Analytics",),
+#         html.P(
+#             children="Temp",
+#         ),
+#         dt.DataTable(
+#             keyword_search_volume.to_dict('records'),
+#             [{"name":i, "id":i} for i in keyword_search_volume.columns]
+#         ),
+#         dt.DataTable(
+#             top_10_related_keywords.to_dict('records'),
+#             [{"name":i, "id":i} for i in top_10_related_keywords.columns]
+#         ),
+#         dt.DataTable(
+#             naver_item_info.to_dict('records'),
+#             [{"name":i, "id":i} for i in naver_item_info.columns]
+#         ),
+#         dt.DataTable(
+#             shopee_item_info.to_dict('records'),
+#             [{"name":i, "id":i} for i in shopee_item_info.columns]
+#         ),
+#         # 그래프		
+#         dcc.Graph(
+#             figure={
+#                 "data": [
+#                     {
+#                         "x": naver_trend["날짜"],
+#                         "y": naver_trend[keyword],
+#                         "type": "lines", "name":"naver"
+#                     },
+#                     {    
+#                         "x": naver_trend["날짜"],
+#                         "y": naver_trend[keyword_en],
+#                         "type": "lines", "name":"google"
+#                     },
+#                 ],
+#                 "layout": {"title": "Title_1"},
+#             },
+#         ),
+#         dt.DataTable(
+#             rising_related_keywords.to_dict('records'),
+#             [{"name":i, "id":i} for i in rising_related_keywords.columns]
+#         ),
+#         dt.DataTable(
+#             top_related_keywords.to_dict('records'),
+#             [{"name":i, "id":i} for i in top_related_keywords.columns]
+#         ),
+#         dt.DataTable(
+#             naver_trend.to_dict('records'),
+#             [{"name":i, "id":i} for i in naver_trend.columns]
+#         ),
+#     ]
+# )
+'''
+시각화 목록
+1. 네이버
+    - 네이버 키워드 검색량 (데이터 프레임)
+    - 연관 키워드 검색량 (데이터 프레임)
+    - 키워드 상품 정보 (데이터 프레임)
+    - 키워드 검색 추이 (구글이랑 같이 교차 그래프)
+2. 쇼피
+    - 쇼피 키워드 상품 정보 (데이터 프레임)
+3. 구글
+    - 키워드 검색 추이 (네이버랑 같이 교차 그래프 (월별 축소 필요))
+    - 떠오르는 연관 키워드 (데이터 프레임)
+    - 상위 연관 키워드 (데이터 프레임)
+'''
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # app.run_server(debug=True)
+    application.debug = True
+    application.run()    
